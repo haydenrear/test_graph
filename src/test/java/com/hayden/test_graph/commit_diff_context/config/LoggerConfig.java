f77c4c877b2af6487f16b3760265e59dfcdbb60f
hayden.rear <hayden.rear@gmail.com>
2024-09-29 16:49:50 -0400

    log sender Sender has contextual information

--- src/test/java/com/hayden/test_graph/commit_diff_context/config/LoggerConfig.java
+++ src/test/java/com/hayden/test_graph/commit_diff_context/config/LoggerConfig.java
@@ -1,13 +1,8 @@
 package com.hayden.test_graph.commit_diff_context.config;
 
-import com.hayden.utilitymodule.ctx.ApplicationContextProvider;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.graphql.client.HttpSyncGraphQlClient;
 
 @Configuration
-@Import(ApplicationContextProvider.class)
 public class LoggerConfig {
 
 }
