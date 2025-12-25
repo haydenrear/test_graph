362551ca6d7de817f8e0e5ea9704fe1a53c97515
hayden.rear <hayden.rear@gmail.com>
2025-12-25 16:46:59 -0500

    removed dgs

--- src/test/java/com/hayden/test_graph/commit_diff_context/config/GraphQlConfig.java
+++ src/test/java/com/hayden/test_graph/commit_diff_context/config/GraphQlConfig.java
@@ -1,9 +1,13 @@
 package com.hayden.test_graph.commit_diff_context.config;
 
 import com.hayden.commitdiffmodel.config.CommitDiffContextConfigProps;
+import com.netflix.graphql.dgs.DgsQueryExecutorConfig;
+import com.netflix.graphql.dgs.GraphQlClientProps;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.graphql.client.DgsGraphQlClient;
 import org.springframework.graphql.client.GraphQlClient;
 import org.springframework.graphql.client.HttpSyncGraphQlClient;
@@ -13,20 +17,8 @@ import org.springframework.web.client.RestTemplate;
 import java.time.Duration;
 
 @Configuration
+@Import(DgsQueryExecutorConfig.class)
 public class GraphQlConfig {
 
-    @Bean
-    public DgsGraphQlClient graphQlClient(
-            CommitDiffContextConfigProps commitDiffContextConfigProps
-    ) {
-        var template = new RestTemplateBuilder();
-        template = template.connectTimeout(Duration.ofSeconds(1000));
-        template = template.readTimeout(Duration.ofSeconds(1000));
-        return DgsGraphQlClient.create(
-                HttpSyncGraphQlClient.builder(RestClient.create(template.build()).mutate().baseUrl(commitDiffContextConfigProps.getUrl()).build())
-                        .blockingTimeout(Duration.ofSeconds(1000))
-                        .url(commitDiffContextConfigProps.getUrl())
-                        .build());
-    }
 
 }
